27b658417912938f521d94b3e14eba0c255ffec0
SGDaniels08 <SGDaniels08@gmail.com>
2026-04-05 22:52:00 -0400

    End lesson 86, refactoring mobx mapping

--- client/src/features/counter/Counter.tsx
+++ client/src/features/counter/Counter.tsx
@@ -1,27 +1,40 @@
 import { Button, ButtonGroup, Typography } from '@mui/material';
 import { useStore } from '../../lib/hooks/useStore';
-import { Observer } from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 
-export default function Counter() {
+const Counter = observer(function Counter() {
     const {counterStore} = useStore();
 
     // Anything inside the <Observer> tags will be able to view our mobx state
     return (
         <>
-        <Observer>
-            {() => (
-                <>
-                    <Typography variant='h4' gutterBottom>{counterStore.title}</Typography>
-                    <Typography variant='h6'>The count is: {counterStore.count}</Typography>
-                </>
-            )}
-        </Observer>
+            <Typography variant='h4' gutterBottom>{counterStore.title}</Typography>
+            <Typography variant='h6'>The count is: {counterStore.count}</Typography>
 
-        <ButtonGroup sx={{mt: 3}}>
-            <Button onClick={() => counterStore.decrement()} variant="contained" color="error">Decrement</Button>
-            <Button onClick={() => counterStore.increment()} variant="contained" color="success">Increment</Button>
-            <Button onClick={() => counterStore.increment(5)} variant="contained" color="primary">Increment by 5</Button>
-        </ButtonGroup>
+            <ButtonGroup sx={{mt: 3}}>
+                <Button onClick={() => counterStore.decrement()} variant="contained" color="error">Decrement</Button>
+                <Button onClick={() => counterStore.increment()} variant="contained" color="success">Increment</Button>
+                <Button onClick={() => counterStore.increment(5)} variant="contained" color="primary">Increment by 5</Button>
+            </ButtonGroup>
         </>
     )
-}
+});
+
+export default Counter
+
+// If we don't declare Counter as a variable to export, have to use more boilerplate:
+//
+// default export observer(function Counter() {
+//     const {counterStore} = useStore();
+//
+//     return (
+//         <>
+//            <Observer>
+//              {() => (
+//                  <>                              
+//                      <Typography variant='h4' gutterBottom>{counterStore.title}</Typography>
+//                      <Typography variant='h6'>The count is: {counterStore.count}</Typography>
+//                  </>
+//              )}
+//            </Observer>
+// ...
--- client/src/lib/stores/counterStore.ts
+++ client/src/lib/stores/counterStore.ts
@@ -1,17 +1,25 @@
 // Sample of how to use mobx
-import { action, makeObservable, observable } from 'mobx';      // two parameters, what object you want to make observable, and a list of properties you want to observe
+import { action, makeAutoObservable, makeObservable, observable } from 'mobx';      // two parameters, what object you want to make observable, and a list of properties you want to observe
 
 export default class CounterStore {
     title = 'Counter store';
     count = 42;
 
+    // makeObservable if you want to have fine-grain control over mobx properties
+    // 
+    // constructor() {
+    //     makeObservable(this, {
+    //         title: observable,
+    //         count: observable,
+    //         increment: action,      // Functons are type "action" in mobx
+    //         decrement: action       // If you don't use arrow functions below, need to use "action.bound"
+    //     })
+    // }
+    // 
+    // makeAutoObservable will handle all of the above automatically
+
     constructor() {
-        makeObservable(this, {
-            title: observable,
-            count: observable,
-            increment: action,      // Functons are type "action" in mobx
-            decrement: action       // If you don't use arrow functions below, need to use "action.bound"
-        })
+        makeAutoObservable(this);
     }
 
     // Inside a class, if we don't use an arrow function like below, we'll have to make sure the method is bound to the class
